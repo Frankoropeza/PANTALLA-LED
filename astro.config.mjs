f4dcf046156ada392cae740cf6561f2ccd7057f1
Frankoropeza <frankoropeza840@gmail.com>
2026-04-13 13:18:38 -0600

    fix: agregar integración @astrojs/tailwind para compilar estilos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://frankoropeza.github.io',
   base: '/PANTALLA-LED',
   output: 'static',
-  vite: {
-    cacheDir: './.vite-cache',
-  },
+  integrations: [tailwind()],
 });
